3267a5c34863b06387308fcac0c99587b96222e5
OVO Transient <Huyucan060830@outlook.com>
2026-01-22 13:22:38 +0800

    change-toc

--- src/config.ts
+++ src/config.ts
@@ -141,8 +141,7 @@ export const siteConfig: SiteConfig = {
                 "有的因为责任而坚持",
                 "有的因为仇恨而坚持",
                 "有的因为精彩而坚持",
-                "有的因为爱情而坚持",
-                "有的因为梦想而坚持...",
+                "有的因为爱情而坚持...",
                 "而方源的回答呢？",
                 "他仍旧面无表情",
                 "毫无所动地向前进",
@@ -193,7 +192,7 @@ export const siteConfig: SiteConfig = {
 	},
 	toc: {
 		enable: true, // 启用目录功能
-		mode: "sidebar", // 目录显示模式："float" 悬浮按钮模式，"sidebar" 侧边栏模式
+		mode: "float", // 目录显示模式："float" 悬浮按钮模式，"sidebar" 侧边栏模式
 		depth: 2, // 目录深度，1-6，1 表示只显示 h1 标题，2 表示显示 h1 和 h2 标题，依此类推
 		useJapaneseBadge: false, // 使用日语假名标记（あいうえお...）代替数字，开启后会将 1、2、3... 改为 あ、い、う...
 	},
